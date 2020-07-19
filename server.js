d7df74c7506a79db9cb4653a4e0649c90c4b8b1e
Stanley-Ugo <iamKosmicblast@gmail.com>
2020-07-20 00:00:11 +0100

    Added '/profile/:id' Endpoint

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const database = {
             joined: new Date()
         },
         {
-            id: '123',
+            id: '124',
             name: 'Sally',
             email: 'Sally@gmail.com',
             password: 'bananas',
@@ -50,6 +50,20 @@ app.post('/register', (req, res) => {
         joined: new Date()
     })
     res.json(database.users[database.users.length -1])
+})
+
+app.get('/profile/:id', (req, res) => {
+    const { id } = req.params;
+    let found = false;
+    database.users.forEach(user => {
+        if (user.id === id) {
+            found = true;
+           return res.json(user);
+        }
+    })
+    if (!found) {
+        res.status(400).json('not found');
+    }
 })
 
 app.listen(3000, ()=> {
