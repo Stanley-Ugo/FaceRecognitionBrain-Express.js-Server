a4954703fbea8fdc6c328868c9f30e2911973fa3
Stanley-Ugo <iamKosmicblast@gmail.com>
2020-07-19 09:42:07 +0100

    Testing The Server - Making a Get request

--- server.js
+++ server.js
@@ -2,4 +2,11 @@ const express = require('express');
 
 const app = express();
 
+app.get('/', (req, res) => {
+    const user = {
+        name : 'Sally',
+        hubby: 'soccer'
+    }
+    res.send(user);
+})
 app.listen(3000);
