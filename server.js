ce16e0453a811e3003a2c55091a6ca7c36d44bf6
Stanley-Ugo <iamKosmicblast@gmail.com>
2020-07-23 00:03:34 +0100

    Created a 'handleProfile' service & injected it into '/profile/:id' route

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const knex = require("knex");
 
 const register = require('./controllers/register');
 const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
 
 const db = knex({
   client: "pg",
@@ -31,21 +32,7 @@ app.post("/signin", (req, res) => { signin.handleSignin(req, res, db, bcrypt)});
 
 app.post("/register",  (req, res) => {register.handleRegister(req, res, db, bcrypt)});
 
-app.get("/profile/:id", (req, res) => {
-  const { id } = req.params;
-
-  db.select("*")
-    .from("users")
-    .where({ id })
-    .then((user) => {
-      if (user.length) {
-        res.json(user[0]);
-      } else {
-        res.status(400).json("Not Found");
-      }
-    })
-    .catch((err) => res.status(400).json("error getting user"));
-});
+app.get("/profile/:id", (req, res) => { profile.handleProfileGet(req, res, db)});
 
 app.put("/image", (req, res) => {
   const { id } = req.body;
